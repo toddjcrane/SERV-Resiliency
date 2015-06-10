944f6a247a3486c0b63e3538bc5695b3b1e10443
Todd Crane <todd@toddcrane.com>
2015-06-09 17:17:50 -0700

    basic skill functionality
    Signed-off-by: Todd Crane <todd@toddcrane.com>

--- SERV Resilliency/DetailViewController.swift
+++ SERV Resilliency/DetailViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
 
+    @IBOutlet weak var skillTitle: UITextField!
+    @IBOutlet weak var skillProtected: UISwitch!
+    @IBOutlet weak var skillBody: UITextView!
 
     var detailItem: AnyObject? {
         didSet {
@@ -21,16 +23,62 @@ class DetailViewController: UIViewController {
     }
 
     func configureView() {
-        // Update the user interface for the detail item.
+        
         if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.valueForKey("timeStamp")!.description
+            self.navigationItem.title = detail.valueForKey("name")!.description
+            if let title = self.skillTitle {
+                title.text = detail.valueForKey("name")!.description
+            }
+
+            if let protected = self.skillProtected {
+                protected.on = detail.valueForKey("protected")!.boolValue
+            }
+            if let body = self.skillBody {
+                body.text = detail.valueForKey("body")!.description
             }
         }
     }
+    /*override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        if (self.editing) {
 
+            if let name = self.skillTitle {
+                name.hidden = false
+                name.enabled = true
+            }
+
+            if let body = self.skillBody {
+                body.editable = true
+            }
+            if let protected = self.skillProtected {
+                protected.hidden = false
+                protected.enabled = true
+            }
+        }
+        else {
+            if let detail = self.detailItem {
+                if let name = self.skillTitle {
+                    name.hidden = true
+                    name.enabled = false
+                    detail.setValue(name.text, forKey: "name")
+                    self.navigationItem.title = detail.valueForKey("name")!.description
+                }
+                if let body = self.skillBody {
+                    body.editable = false
+                    detail.setValue(body.text, forKey: "body")
+                    
+                }
+                if let protected = self.skillProtected {
+                    protected.hidden = true
+                    protected.enabled = false
+                    detail.setValue(protected.on, forKey: "protected")
+                }
+            }
+        }
+    }*/
     override func viewDidLoad() {
         super.viewDidLoad()
+        //self.navigationItem.rightBarButtonItem = self.editButtonItem()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
--- SERV Resilliency/MasterViewController.swift
+++ SERV Resilliency/MasterViewController.swift
@@ -18,7 +18,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
@@ -45,7 +45,10 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
              
         // If appropriate, configure the new managed object.
         // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-        newManagedObject.setValue(NSDate(), forKey: "timeStamp")
+        newManagedObject.setValue("Unnamed Skill", forKey: "name")
+        newManagedObject.setValue("Body goes here", forKey: "body")
+        newManagedObject.setValue(false, forKey: "protected")
+        newManagedObject.setValue(false, forKey: "read")
              
         // Save the context.
         do {
@@ -112,7 +115,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) {
         let object = self.fetchedResultsController.objectAtIndexPath(indexPath)
-        cell.textLabel!.text = object.valueForKey("timeStamp")!.description
+        cell.textLabel!.text = object.valueForKey("name")!.description
     }
 
     // MARK: - Fetched results controller
@@ -131,7 +134,7 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timeStamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
